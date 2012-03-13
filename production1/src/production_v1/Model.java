727e55e2fed8d09e758ea67772290cc7601fcce6
Mackro Otterberg <spamiuso@gmail.com>
2012-03-13 17:44:15 +0100

    First real files

---
+++ production1/src/production_v1/Model.java
@@ -0,0 +1,23 @@
+package production_v1;
+
+public class Model {
+	
+	public Model (){
+		
+	}
+	
+	public boolean attackCompatible(Territory t1, Territory t2){
+		if(!t1.hasConnection(t2) || t1.getOwner().getID() == t2.getOwner().getID()){
+			return false;
+		}
+		return true;
+	}
+	
+	public boolean moveCompatible(Territory t1, Territory t2){
+		if(!t1.hasConnection(t2) || t1.getOwner().getID() != t2.getOwner().getID()){
+			return false;
+		}
+		return true;
+	}
+
+}
